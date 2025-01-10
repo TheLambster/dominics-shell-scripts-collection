4089c68df70bdbcd128d98389cb38336ef610ca3
The Lambster <97347052+TheLambster@users.noreply.github.com>
2025-01-10 13:02:58 -0600

    Update and rename stream.sh to ffmpeg-stream-script.sh

--- ffmpeg-stream-script.sh
+++ ffmpeg-stream-script.sh
@@ -1,2 +1,2 @@
-YT_KEY=qh7b-yt2g-yry5-ecwk-80w6
+YT_KEY=your-key
 ffmpeg -f x11grab -video_size 1920x1080 -framerate 30 -probesize 10M -i $DISPLAY -f pulse -ac 2 -i default -acodec aac -ar 44100 -f flv rtmp://a.rtmp.youtube.com/live2/$YT_KEY
